b7a29474a8848e22559470c7a4b8ef59877adc96
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-04-26 20:33:23 +0100

    update

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
-/**
- * main - Prints out the last part of a quote in the standard error.
- * Return: 1.
- */
-int main(void)
-{
-    write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-    return (1);
-}
+#!/usr/bin/python3
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
