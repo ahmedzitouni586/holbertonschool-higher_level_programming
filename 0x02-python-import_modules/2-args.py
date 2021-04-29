5e2b7a8e2369802a77e8f3feabbe897cf6d84144
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-04-29 21:26:04 +0100

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    sys.argv.pop(0)
+    print(f"{len(sys.argv)} argument:")
+    for i, arg in enumerate(sys.argv):
+        print(f"{i}: {arg}")
