135a22878a4e3d4b7039dfa657d29036a2742276
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-04-29 21:50:23 +0100

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 import sys
 if __name__ == "__main__":
     sys.argv.pop(0)
-    if sys.argv == 0:
+    if len(sys.argv) == 0:
         print("0 arguments.")
     else:
         if len(sys.argv) == 1:
