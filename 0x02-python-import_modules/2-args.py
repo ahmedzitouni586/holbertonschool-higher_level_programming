ee76d8081c0e5353c8f750e47be99e45569340fd
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-04-29 21:45:38 +0100

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,10 @@ if __name__ == "__main__":
     if sys.argv == 0:
         print("0 arguments.")
     else:
-        print(f"{len(sys.argv)} arguments:")
-        for i, arg in enumerate(sys.argv):
-            print(f"{i}: {arg}")
+        if len(sys.argv) == 1:
+            print("1 argument:")
+            print(f"1: {sys.argv[0]}")
+        else:
+            print(f"{len(sys.argv)} arguments:")
+            for i, arg in enumerate(sys.argv):
+                print(f"{i+1}: {arg}")
