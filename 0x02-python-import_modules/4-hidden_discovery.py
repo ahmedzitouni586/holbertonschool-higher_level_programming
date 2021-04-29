09d35b3c422ed6cd84c45e2415618061318bfadd
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-04-29 22:31:38 +0100

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from hidden_4 import *
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    for i in names:
-        if i.startswith("__" , 0, 2):
-            print(i)
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print("{:i}".format(i))
