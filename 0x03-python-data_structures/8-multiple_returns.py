f3d3cacd56c4cfc2cdd82d7a86141fbbe1fbf9ce
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-05-05 13:31:05 +0100

    multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def multiple_returns(sentence):
     len_sentence = len(sentence)
     if (sentence == 0):
-        return (0. None)
+        return (0, None)
     return (len_sentence, sentence[0])
