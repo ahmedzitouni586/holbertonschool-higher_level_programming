429281e0779d060b542e1981626b3e356aa1fb65
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-05-18 21:55:16 +0100

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class to define a square"""
+
+
+class Square:
+    """square"""
+    def __init__(self, size):
+        """private instance attribute"""
+        self.__size = size
