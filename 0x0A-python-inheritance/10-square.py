4b7ab186100e6ed5958d6ce976cf01b652e93ec6
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-06-02 00:03:15 +0100

    update

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""holberton module"""
-Square = __import__('9-rectangle.py').Rectangle
+""" holberton """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """class Square that inherits from Rectangle"""
+    """ class to define a square """
+
     def __init__(self, size):
-        """Instantiation with size"""
-        self.__size = size
         self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
