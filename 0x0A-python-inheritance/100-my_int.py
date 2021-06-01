c4f0ef9957f8f0093a0c77b6c30704ebad030d13
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-06-02 00:13:31 +0100

    my_int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,9 +5,7 @@
 class MyInt(int):
     """define Myint class"""
     def __eq__(self, other):
-        """define elquality"""
         return not super().__eq__(other)
 
     def __ne__(self, other):
-        """define elquality"""
         return not super().__ne__(other)
