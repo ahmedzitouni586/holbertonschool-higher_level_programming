8caa963b6d7637b2a9f209868aff99c6ae17d04a
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-06-01 23:50:02 +0100

    rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""holberton module"""
+""" module for rectange class """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """class Rectangle"""
+    """ class rectangle """
 
     def __init__(self, width, height):
-        """Instantiation with width and height"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        """function that find area of rectangle"""
         return (self.__width * self.__height)
 
-     def __str__(self):
+    def __str__(self):
         return ("[Rectangle] {}/{}".format(self.__width, self.__height))
