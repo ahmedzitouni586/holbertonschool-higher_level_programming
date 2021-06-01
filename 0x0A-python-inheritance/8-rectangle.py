9b15901cefb01e499436d9e3a0d06a36c38985d1
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-06-01 23:37:25 +0100

    rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """holberton module"""
-Rectangle = __import__('7-base_geometry.py').BaseGeometry
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
 
 
 class Rectangle(BaseException):
     """class Rectangle"""
     def __init__(self, width, height):
 
-        
+
         """Instantiation with width and height"""
         self.__width = width
         self.__height = height
