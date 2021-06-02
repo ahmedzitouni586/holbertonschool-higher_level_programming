79170a2e654ef9dcd94f254122b659cae814270f
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-06-02 23:23:50 +0100

    fixing code

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 
 
 def append_write(filename="", text=""):
-    """write a string at the end of the file"""
+    """ appends a string at the end of a text file"""
     with open("filename", mode='a', encoding='UTF8') as ff:
         ff.write(text)
     return len(text)
