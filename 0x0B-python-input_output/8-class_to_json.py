02dcf4bf4b07c4902c7c20bf37ef52f25729ec0b
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-06-02 23:34:15 +0100

    fixing

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """manipulate files"""
-import json
 
 
 def class_to_json(obj):
-    """return dictionary description with simple data structure """
+    """returns the dictionary description with simple data structure 
+    (list, dictionary, string, integer and boolean) for JSON serialization of an object"""
     return obj.__dic__
