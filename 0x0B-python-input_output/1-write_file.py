57e757f7b3af8b4abda157f2617db7b6eb8deb68
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-06-02 23:19:10 +0100

    fixing code

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""manipulate files"""
+"""Write to a file"""
 
 
 def write_file(filename="", text=""):
-    """function to write in a file"""
+    """write a string and return the number of characters"""
     with open("filename", encoding="UTF8") as ff:
         return ff.write(text)
