7f5bdbbe1a4c696d8008854ea73ef17eff3d2324
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-06-09 00:05:50 +0100

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-""" define a Rectagle class"""
+"""module to define a Rectagle class"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """class constractor"""
+    """define rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """init method for rectangle class"""
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        super().__init__(id)
 
     @property
     def width(self):
@@ -66,13 +67,13 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
 
     def area(self):
-        """ return the area of the rectangle"""
+        """calculate the area of the rectangle"""
         return self.height * self.width
 
     def display(self):
         """prints in stdout the Rectangle instance with the character #"""
         for y in range(self.__y):
-            print()
+            print("")
         for height in range(self.__height):
             for x in range(self.__x):
                 print(" ", end="")
@@ -85,3 +86,53 @@ class Rectangle(Base):
         R = "[Rectangle] {} {}/{}".format(self.id, self.x, self.y)
         return R + "- {}/{}".format(self.width, self.height)
 
+    def update(self, *args, **kwargs):
+        """ Assigns a key/value argument to each attribute """
+        if args and len(args) != 0:
+            try:
+                if args[0] is not None:
+                    self.id = args[0]
+            except IndexError:
+                return
+            try:
+                if args[1] is not None:
+                    self.width = args[1]
+            except IndexError:
+                return
+            try:
+                if args[2] is not None:
+                    self.height = args[2]
+            except IndexError:
+                return
+            try:
+                if args[3] is not None:
+                    self.x = args[3]
+            except IndexError:
+                return
+            try:
+                if args[4] is not None:
+                    self.y = args[4]
+            except IndexError:
+                return
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        dict = {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+        return dict
