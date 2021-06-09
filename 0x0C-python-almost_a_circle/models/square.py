727623d351bd9573440060114be4da096968d094
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-06-09 18:28:31 +0100

    square!

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     """ define square class """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ square class """
+        """ square constractor """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
