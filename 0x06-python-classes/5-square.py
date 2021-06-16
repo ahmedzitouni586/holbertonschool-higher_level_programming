d0921d392c5c042c3901874968fed07eee260c49
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-06-16 10:37:14 +0100

    print square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -33,7 +33,7 @@ class Square:
     def my_print(self):
         """ draw square """
         for i in range(0, self.__size):
-            for j in range(0, self.__size):
+            for j in range(self.__size):
                 print("#", end="")
                 print()
         if self.__size == 0:
