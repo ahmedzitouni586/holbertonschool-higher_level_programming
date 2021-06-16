f56d30b5d9b99ec2a1c3cc99a92cf748c76be1dd
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-06-16 09:46:05 +0100

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Square Module  """
+
+
+class Square:
+    """ class Square """
+    def __init__(self, size):
+        """ class initialization """
+        self.__size = size
