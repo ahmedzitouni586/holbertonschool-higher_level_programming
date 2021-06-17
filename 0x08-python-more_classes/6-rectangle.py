83bedb847ea073e9bf0a81b6f7146ffaa51c81e8
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-06-17 12:25:31 +0100

    rectangle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """ define rectangle class """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ Instantiation class """
         self.width = width
