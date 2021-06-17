5182089d71a8633994dc8198a63eb07410fa9268
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-06-17 10:06:51 +0100

    rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""This is a docstrings.
-    for a new class: Rectangle.
-"""
+""" module to define rectangle class """
 
 
 class Rectangle:
-    """this is an empty class
-        that defines a Rectangle
-    """
+    """ define rectngle class """
     pass
