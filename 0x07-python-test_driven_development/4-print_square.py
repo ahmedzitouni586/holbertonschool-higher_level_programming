269d3906c77b802d6de3f81b6184a2f352589dda
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-07-02 23:40:40 +0100

    square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""
-Holberton test module
-"""
-
-
 def print_square(size):
-    """
-    prints a square with the character #
-    """
+    """  prints a square with the character # """
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    elif size < 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
-    else:
-        for i in range(size):
-            for j in range(size):
-                print('#', end="")
-            print("")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
