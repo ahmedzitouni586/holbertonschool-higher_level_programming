134fbc0ba3030a93d3338f0f86764f7c42435fa8
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-08-02 22:27:09 +0100

    update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-if (!parseInt(process.argv[2])) {
-    console.log('Missing size');
-}
-else if (process.argv[2] > 0) {
-    for (let i = 0; i < process.argv[2]; i++) {
-        let row = ''
-        for (let j = 0; j < process.argv[2]; j++) {
-            row+='X'
-        }
-        console.log(row+" ");
-    }
+let x = process.argv[2];
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
 }
