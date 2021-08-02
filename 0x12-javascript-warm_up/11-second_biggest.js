df23ec2b208711493a24d93d0117ace93430aae6
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-08-02 22:46:44 +0100

    update

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+if (process.argv.length === 2 || process.argv.length === 3) {
+    console.log('0');
+  } else {
+    const array = process.argv.slice(2).sort((a, b) => a - b);
+    const len = array.length;
+  
+    console.log(array[len - 2]);
+  }
