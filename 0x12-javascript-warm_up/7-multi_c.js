37872603e5bd4b6a3095e5beb2041a0bfe61d933
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-08-02 22:16:29 +0100

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-if (!parseInt(process.argv[2])) {
-    console.log('Missing number of occurrences');
-}
-else if (process.argv[2] > 0) {
-    for (let i = 0; i <= process.argv[2]; i++) {
+if (process.argv[2]) {
+    for (let i = 0; i < process.argv[2]; i++) {
         console.log('C is fun');
     }
+} else {
+    console.log('Missing number of occurrences');
 }
