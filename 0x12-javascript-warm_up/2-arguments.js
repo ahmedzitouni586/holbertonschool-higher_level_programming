bdc2849d92ebbaec903745fc54977be77351a021
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-08-02 21:17:22 +0100

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@ const arg = process.argv;
 const len = arg.length;
 if (len === 2) {
     console.log("No argument")
+} else if (len === 3) {
+    console.log('Argument found');
 } else {
-    console.log("Argument found")
+    console.log("Argument found");
 }
