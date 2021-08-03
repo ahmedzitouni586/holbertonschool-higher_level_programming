375bc7a035120d15d6d7f73dffef99c32c23fab9
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-08-03 22:45:23 +0100

    first commit

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const nSquare = require('./5-square');
+module.exports = class Square extends nSquare {
+    charPrint(c) {
+        if (c == null) {
+            super.print();
+        }
+        else {
+            for (let i = 0; i < this.width; ++i) {
+                console.log(c.repeat(this.width));
+            }
+        }
+        
+    }
+}
---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+    let tmp;
+    for (let start = 0, end = list.length - 1; start < end; ++start, --end) {
+        tmp = list[start];
+        list[start] = list[end];
+        list[end] = tmp;
+    }
+    return (list);
+};
