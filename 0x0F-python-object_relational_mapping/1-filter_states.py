b9c789a675c169047b4b7e6a3a3752aa88805ebc
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-08-07 13:47:04 +0100

    name start with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 """
-Lists all states from the database hbtn_0e_0_usa
+Lists states start with N from the database hbtn_0e_0_usa
 """
 
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
-    
+
     with MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
                          db=argv[3], port=3306) as db:
         db.execute("SELECT *\
