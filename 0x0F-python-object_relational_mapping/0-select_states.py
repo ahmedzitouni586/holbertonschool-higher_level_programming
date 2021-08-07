3a2c252ff8463184d8982254f60ba48746421286
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-08-07 13:35:12 +0100

    first commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     with MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
                          db=argv[3], port=3306) as db:
-        db.execute("SELECT * FROM states ORDER BY id ASC LIKE %N")
+        db.execute("SELECT * FROM states ORDER BY id ASC")
         table = db.fetchall()
         for data in table:
             print(data)
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
-"""Lists all states from the database hbtn_0e_0_usa"""
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
+
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                         db=argv[3], port=3306)
-
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC LIKE 'N%'")
-    data = cursor.fetchall()
-    for row in data:
-        print(row)
+    
+    with MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                         db=argv[3], port=3306) as db:
+        db.execute("SELECT * FROM states ORDER BY id ASC LIKE %N")
+        table = db.fetchall()
+        for data in table:
+            print(data)
