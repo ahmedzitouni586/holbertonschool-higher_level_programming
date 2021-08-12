ac87bca39ab96ab0baa40bc85b31038340de18e4
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-08-12 17:33:52 +0100

    safe from sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+ takes in an argument and displays all values in the states table
+"""
+
+
+def main():
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+    curss = db.cursor()
+    curss.execute("SELECT * FROM states WHERE name = %s\
+    ORDER BY states.id ASC", (argv[4], ))
+    table = curss.fetchall()
+    for row in table:
+        print(row)
+    curss.close()
+    db.close()
+if __name__ == "__main__":
+    main()
