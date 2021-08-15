35915c812b804bfa8740ca306217fad1991b88e0
ahmedzitouni586 <77970176+ahmedzitouni586@users.noreply.github.com>
2021-08-15 19:32:01 +0100

    update

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,25 +1,26 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
-        
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    print () {
-        for (let i = 0; i < this.height; ++i) {
-          console.log('X'.repeat(this.width));
-        }
-    }
+  }
 
-    rotate() {
-        const temp = this.width;
-        this.width = this.height;
-        this.height = temp;
+  print () {
+    for (let i = 0; i < this.height; ++i) {
+      console.log('X'.repeat(this.width));
     }
-    double() {
-        this.height = this.height * 2;
-        this.width = this.width * 2;
-    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
 };
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 const nSquare = require('./5-square');
 module.exports = class Square extends nSquare {
-    charPrint(c) {
-        if (c == null) {
-            super.print();
-        }
-        else {
-            for (let i = 0; i < this.width; ++i) {
-                console.log(c.repeat(this.width));
-            }
-        }
-        
+  charPrint (c) {
+    if (c == null) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.width; ++i) {
+        console.log(c.repeat(this.width));
+      }
     }
-}
+  }
+};
