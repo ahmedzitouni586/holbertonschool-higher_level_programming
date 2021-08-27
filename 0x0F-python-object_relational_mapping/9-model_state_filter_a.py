2182faf1b9feefd1da68296630642c7bad3110d4
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-08-27 14:55:33 +0100

    update

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-lists States objects that contins "a" from the database hbtn_0e_6_usa
-"""
+"""lists States objects that contins "a" from the database hbtn_0e_6_usa"""
 
 
 from sqlalchemy import create_engine
@@ -11,15 +9,17 @@ import model_state
 from model_state import Base, State
 import sys
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+def list():
+    """ list the states """
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3])
+                           )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id)
+    for state in states:
+        if 'a' in state.name.lower():
+            print("{}: {}".format(state.id, state.name))
 
-    session = Session(engine)
-    rows = session.query(State).filter(
-                              State.name.like('%a%')).order_by(State.id)
-    for i in rows:
-        print("{}: {}".format(i.id, i.name))
-    session.close()
+if __name__ == '__main__':
+    list()
