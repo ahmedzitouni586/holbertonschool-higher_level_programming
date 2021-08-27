fae9bd5b0e29dd445a2e0111bb3397f9329366e5
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-08-27 11:54:44 +0100

    filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
 """
  takes in an argument and displays all values in the states table
+ where name matches the argument.
 """
 
-
-def main():
+if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    db = MySQLdb.connect(host="localhost",
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
-                         port=3306)
-    curss = db.cursor()
-    curss.execute("SELECT * FROM states WHERE BINARY name = '{:s}'\
-    ORDER BY id ASC".format(argv[4]))
-    table = curss.fetchall()
-    for row in table:
-        print(row)
-    curss.close()
-    db.close()
-if __name__ == "__main__":
-    main()
+    with MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                         db=argv[3], port=3306) as db:
+        db.execute("SELECT * FROM states WHERE\
+        BINARY name = '{:s}'  ORDER BY states.id".format(argv[4]))
+        table = db.fetchall()
+        for data in table:
+            print(data)
