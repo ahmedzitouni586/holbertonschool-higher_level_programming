e55b091d8f4099d5e47f8bc943ecbd3a1112bfc1
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-08-27 17:33:17 +0100

    first commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status"""
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     import urllib.request
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         url = response.read()
+    print("Body response:")
     print('\t- type: {}'.format(type(url)))
     print('\t- content: {}'.format(url))
     print('\t- utf8 content: {}'.format(url.decode('utf-8')))
