ed4ff2ca3331794edeab4e4e6df1e6a1b47d6f83
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-08-27 15:00:45 +0100

    update

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""lists States objects that contins "a" from the database hbtn_0e_6_usa"""
+"""
+lists States objects that contins 'a' from the database hbtn_0e_6_usa
+"""
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
@@ -14,9 +16,9 @@ def list_all():
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).order_by(State.id)
-    for state in states:
-        if 'a' in state.name.lower():
-            print("{}: {}".format(state.id, state.name))
+    for data in states:
+        if 'a' in data.name.lower():
+            print("{}: {}".format(data.id, data.name))
 
 if __name__ == '__main__':
     list_all()
