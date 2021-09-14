6497a73da950b71eebbe7d04d5b35f8ec07d484b
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-09-14 08:02:11 -0700

    writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,10 +3,9 @@ const fs = require('fs');
 const file = process.argv[2];
 const str = process.argv[3];
 
-fs.readFile(file, 'utf8', (err, data) => {
+fs.readFile(file, str,'utf8', (err, data) => {
   if (err) {
     console.log(err);
     return;
   }
-  console.log(str);
 });
