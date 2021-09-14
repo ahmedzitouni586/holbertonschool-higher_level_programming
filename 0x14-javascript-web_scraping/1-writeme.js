7f270a622529840aacaccbebab660f8d84644e0a
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-09-14 08:12:53 -0700

    update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const file = process.argv[2];
 const str = process.argv[3];
 
-fs.writeFile(file, str,'utf-8', function (err) {
+fs.writeFile(file, str, 'utf-8', function (err) {
   if (err) {
     console.log(err);
   }
