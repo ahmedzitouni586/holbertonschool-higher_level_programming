cfc8739d3bfb987e88f7e40ebd625aa67415ff28
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-09-14 08:33:46 -0700

    starwars_count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+let count = 0;
+
+request.get(process.argv[2], function (error, response, body) {
+  if (error) console.log(error);
+  else {
+    for (const film of JSON.parse(body).results) {
+      for (const character of film.characters) {
+        if (character.includes('18')) ++count;
+      }
+    }
+    console.log(count);
+  }
+});
