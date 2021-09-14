05caa951cba2a50cb6eb8437cfaab0b24af9cdfa
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-09-14 08:05:51 -0700

    writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-const str = process.argv[3];
-
-fs.writeFile(file, str,'utf8', function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
+const args = process.argv;
+const filename = args[2];
+const text = args[3];
+fs.writeFile(filename, text, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
