1f542a740a58a7fae95dec2395d6457f11c46301
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-09-14 06:30:50 -0700

    fixed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-// script that reads and prints the content of a file
-
 const fs = require('fs');
-const file = process.argv[2]
-
-fs.readFile(file, 'utf8', function (err, data) {
+const args = process.argv;
+const file = args[2];
+fs.readFile(file, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
     return;
