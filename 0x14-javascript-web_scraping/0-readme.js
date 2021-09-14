16c141bbfe57bf977c82a1d30954010be7216d43
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-09-14 06:27:46 -0700

    fixed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
     return;
